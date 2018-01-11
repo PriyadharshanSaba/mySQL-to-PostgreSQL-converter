6a024691311073f56c885e9ba10c187fa4be31d6
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-11 22:29:05 +0530

    slp

---
+++ slp.py
@@ -0,0 +1,14 @@
+#Structured Query Language Processing
+#Creator: Priyadharshan Saba
+#Code: Python 3
+#mySQL Processing
+
+from __future__ import unicode_literals
+import numpy
+
+def slp():
+ return 0
+
+
+
+
